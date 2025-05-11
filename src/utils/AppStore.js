dfcd1d02d3d8144a00a2cc1b985fa467bf37755d
Ayush-CS-19 <145976736+Ayush-CS-19@users.noreply.github.com>
2025-05-11 16:35:26 +0530

    Delete src directory

--- src/utils/AppStore.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import cartReducer from "./Slice";
-const appStore = configureStore({
-  reducer: {
-    cart: cartReducer,
-  },
-});
-export default appStore;
